e9a4136d82a3aac395f15b9cb206563eadb23f87
Spy|He|row <42042495+Spys@users.noreply.github.com>
2018-08-04 18:26:03 +0200

    Update Ninja.js

--- .gitignore/Ninja.js
+++ .gitignore/Ninja.js
@@ -5,6 +5,18 @@ var prefix = "!";
 var client = new Discord.Client();
   
 client.on("message",async function(message) {
+if (message.author.equals(bot.user)) return;
+
+if (!message.content.startsWith(prefix)) return;
+
+var args = message.content.substring(prefix.length).split (" ");
+
+var args2 = message.content.split(" ").slice(1);
+
+var guild = message.guild;
+
+var member = message.member;
+
  switch(args[0].toLowerCase()) {
   case"aide":
   var aide_embed = new Discord.RichEmbed
@@ -13,8 +25,7 @@ client.on("message",async function(message) {
    .addField("teste 2","ceci et un test pour les future")
 message.channel.send(aide_embed)
 break;
- }
-
 })
+}
 
 client.login(process.env.TOKEN)
