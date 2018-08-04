3dd15b12ea9e3e6901c05efafddeba368f84f602
Spy|He|row <42042495+Spys@users.noreply.github.com>
2018-08-04 10:38:37 +0200

    Update Ninja.js

--- .gitignore/Ninja.js
+++ .gitignore/Ninja.js
@@ -4,18 +4,17 @@ var prefix = "n'";
 var bot = new Discord.Client();
 var client= new Discord.Client();
 
-client.on("ready", () => {   
+client.on("ready", () => {    
     client.user.setActivity("Loading...")
     console.log("Je suis connecté");
-});
-
+    
 bot.login(process.env.TOKEN)
 
 if(message.content === prefix + "aide"){
     
-     var aide_embed = new Discord.RichEmbed()
+    var aide_embed = new Discord.RichEmbed()
      .setColor("#000101")
      .setTitle("Toutes mes command sont ici!")
      .setDescription("ces command Peuves etre modifier")
      .addField("n'aide", "Affiche Toutes command du Bot!")
-}
+}});
