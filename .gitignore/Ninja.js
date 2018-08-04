f1bc52773f99752098ea370884a9837ad5f855ba
Spy|He|row <42042495+Spys@users.noreply.github.com>
2018-08-04 10:31:44 +0200

    Update Ninja.js

--- .gitignore/Ninja.js
+++ .gitignore/Ninja.js
@@ -10,3 +10,11 @@ client.on("ready", () => {
 });
 
 bot.login(process.env.TOKEN)
+
+if(message.content === prefix + "aide"){
+    var aide_embed = new Discord.RichEmbed()
+     .setColor("#000101")
+     .setTitle("Toutes mes command sont ici!")
+     .setDescription("ces command Peuves etre modifier")
+     .addField("n'aide", "Affiche Toutes command du Bot!")
+}
