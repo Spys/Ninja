8d4ad24c731e50ee41253774017aca7bcaea6cb2
Spy|He|row <42042495+Spys@users.noreply.github.com>
2018-08-04 10:23:12 +0200

    Update Ninja.js

--- .gitignore/Ninja.js
+++ .gitignore/Ninja.js
@@ -4,9 +4,9 @@ var prefix = "n'";
 var bot = new Discord.Client();
 var client= new Discord.Client();
 
-client.on("ready", () => {    
-    console.log("Je suis connecté");
+client.on("ready", () => {   
     client.user.setActivity("Loading...")
+    console.log("Je suis connecté");
 });
 
 bot.login(process.env.TOKEN)
