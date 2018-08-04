01b717ff7abea4ea5668c2c8baa81262e806f7f8
Spy|He|row <42042495+Spys@users.noreply.github.com>
2018-08-04 23:17:38 +0200

    Update Ninja.js

--- .gitignore/Ninja.js
+++ .gitignore/Ninja.js
@@ -5,15 +5,25 @@ var prefix = "!";
 var client = new Discord.Client();
   
 client.on("message", async function(message) {
+    if (message.author.equals(client.user)) return;
 
+    if (!message.content.startsWith(prefix)) return;
+    
+    var args = message.content.substring(prefix.length).split (" ");
+    
+    var args2 = message.content.split(" ").slice(1);
+    
+    var guild = message.guild;
+    
+    var member = message.member;
 switch(args[0].toLowerCase()) {
   case"aide":
-  var aide_embed = new Discord.RichEmbed()
-   .setTitle("Bienvenue sur la command !aide")
-   .setDescription("toutes les comand se retrouverons juste ici :)")
-   .addField("!aide", "Vous affiche Toutes les comand du bot")
-message.channel.send(aide_embed)
+   var aide_embed = new Discord.RichEmbed()
+    .setTitle("Bienvenue sur la command !aide")
+    .setDescription("toutes les comand se retrouverons juste ici :)")
+    .addField("!aide", "Vous affiche Toutes les comand du bot")
+  message.channel.send(aide_embed)
 break;
-}
+ }
 })
-client.login(process.env.TOKEN)
+           client.login(process.env.TOKEN)
