15192c8ecc4d18c537be4e8fbfc0fad95cc361e6
Spy|He|row <42042495+Spys@users.noreply.github.com>
2018-08-05 01:01:26 +0200

    Update Ninja.js

--- .gitignore/Ninja.js
+++ .gitignore/Ninja.js
@@ -3,7 +3,31 @@ const Discord = require('discord.js');
 var prefix = "n-";
 
 var client = new Discord.Client();
-  
+
+client.on(`message`, message => {
+    let command = message.content.split(" ")[0];
+    const args = message.content.slice(prefix.lenght).split(/ +/);
+    command = args.shift().toLowerCase();
+
+    if (command === "kick") {
+        let modRole = message.guild.roles.find("name", "test");
+        if(!message.member.roles.has(modRole.id)) {
+            return message.reply("tu n'as pas la permission pour utiliser cette command.").catch(console.error);
+}          
+if(message.mentions.users.size ===0) {
+    return message.reply("Merci de mentionner l'utilisateur a expulser.").catch(console.error);
+}
+let kickMember = message.guild.member(message.mentions.users.first());
+id(!kickMember) {
+    return message.reply("Cet utilisateurs et introuvable ou impossible a expulser.")
+}
+if(!message.guild.member(client.users).hasPermission("KICK_MEMBERS")) {
+    return message.reply("je n'ai pas la permission Kick_MEMBERS pour faire ceci.").catch(console.error);
+}
+kickMember.kick().then(member => {
+    message.reply(`${member.user.username} a ete expulse avec succes.`).catch(console.error);
+    message.guild.channels.send(`**${member.user.username} a ete expulser du discord par **${message.author.username}**`)
+}).catch(console.error);
 client.on("message", async function(message) {
     if (message.author.equals(client.user)) return;
 
@@ -41,14 +65,10 @@ client.on("message", async function(message) {
     
     var member = message.member;
 switch(args[0].toLowerCase()) {
-    case"infoserv":
-     var infoserv_embed = new Discord.RichEmbed()
-      .setColor("#F401FD")
-      .setTitle("Command infoserv")
-      .setDescription("cette comand Vous permet de voir les Statistiques de votre Serveur")
-      .addField("Nombre de personnes", `il y a ${memberCount} Personnes sur le Serveur`)
-      message.channel.send(infoserv_embed)
+    case"serverlist":
+    message.channel.send(client.guild.map(r => r.name + `| *${r.memberCount}* membre`))
 break;
 }
-})
+},)}},)
+
 client.login(process.env.TOKEN)
