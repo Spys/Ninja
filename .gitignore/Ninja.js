7418406f5ce7ddaf93bb9dc13fde47774c081f3e
Spy|He|row <42042495+Spys@users.noreply.github.com>
2018-08-04 18:33:21 +0200

    Update Ninja.js

--- .gitignore/Ninja.js
+++ .gitignore/Ninja.js
@@ -22,7 +22,7 @@ var member = message.member;
   var aide_embed = new Discord.RichEmbed
    .setTitle("test !")
    .setDescription("test")
-   .addField("teste 2","ceci et un test pour les future")
+   .addField("teste 2", "ceci et un test pour les future")
 message.channel.send(aide_embed)
 break;
 })
