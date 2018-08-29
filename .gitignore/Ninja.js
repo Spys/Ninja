96642ded1f29f9a7256f6d02522d19403d57a981
Spy|He|row <42042495+Spys@users.noreply.github.com>
2018-08-30 00:39:15 +0200

    Update Ninja.js

--- .gitignore/Ninja.js
+++ .gitignore/Ninja.js
@@ -1,33 +1,38 @@
-const Discord = require('discord.js');
+
  
 var prefix = "n-";
  
 var client = new Discord.Client();
  
+client.on(`ready`, function () {
+    console.log("Je suis connecter !");
+})
+ 
 client.on(`message`, message => {
     let command = message.content.split(" ")[0];
     const args = message.content.slice(prefix.lenght).split(/ +/);
  
- if (message.content === prefix + "kick") {
+    if (message.content === prefix + "kick") {
         let modRole = message.guild.roles.find("name", "test");
         if(!message.member.roles.has(modRole.id)) {
-            return message.reply(":cop: *tu n'as pas la **permission** pour utiliser cette command.*").catch(console.error);
-if(message.mentions.users.size ===0) {
-    return message.reply(":shrug: *Merci de mentionner **l'utilisateur** a expulser.*").catch(console.error);
-}
- 
-let kickMember = message.guild.member(message.mentions.users.first())
-id(!kickMember); {
-    return message.reply("*Cet utilisateurs et **introuvable ou impossible** a expulser.* :thinking: ")
-}
-if(!message.guild.member(client.users).hasPermission("KICK_MEMBERS")) {
-    return message.reply("*je n'ai pas la permission Kick_MEMBERS pour faire ceci.* :thumbsdown: ").catch(console.error);
-}
-kickMember.kick().then(member => {
-    message.reply(`${member.user.username} a ete expulse avec succes.`).catch(console.error);
-    message.guild.channels.send(` :warning: **${member.user.username}** a ete expulser du discord par **${message.author.username}**`).catch(console.error);
-});
-}}
+        return message.reply(":cop: *tu n'as pas la **permission** pour utiliser cette command.*").catch(console.error);
+            if(message.mentions.users.size ===0) {
+                return message.reply(":shrug: *Merci de mentionner **l'utilisateur** a expulser.*").catch(console.error);
+            }
+            let kickMember = message.guild.member(message.mentions.users.first())
+            id(!kickMember); {
+                return message.reply("*Cet utilisateurs et **introuvable ou impossible** a expulser.* :thinking: ")
+            }
+            if(!message.guild.member(client.users).hasPermission("KICK_MEMBERS")) {
+                return message.reply("*je n'ai pas la permission Kick_MEMBERS pour faire ceci.* :thumbsdown: ").catch(console.error);
+            }
+            kickMember.kick().then(member => {
+                message.reply(`${member.user.username} a ete expulse avec succes.`).catch(console.error);
+                message.guild.channels.send(` :warning: **${member.user.username}** a ete expulser du discord par **${message.author.username}**`).catch(console.error);
+            });
+        }
+    }
+})
  
 client.on("message", async function(message) {
     if (message.author.equals(client.user)) return;
@@ -41,21 +46,22 @@ client.on("message", async function(message) {
     var guild = message.guild;
    
     var member = message.member;
- switch(args[0].toLowerCase()) {
-  case"aide":
-   var aide_embed = new Discord.RichEmbed()
-    .setColor("#F401FD")
-    .setTitle("Bienvenue sur la command !aide")
-    .setDescription("toutes les comand se retrouverons juste ici :)")
-    .addField("n-aide", "Vous affiche Toutes les comand du bot")
-    .addField("n-infoserv", "le bot vous donne quelque information utile sur le serveur!")
-  message.channel.send(aide_embed)
-break;
+ 
+    switch(args[0].toLowerCase()) {
+    case"aide":
+    var aide_embed = new Discord.RichEmbed()
+        .setColor("#F401FD")
+        .setTitle("Bienvenue sur la command !aide")
+        .setDescription("toutes les comand se retrouverons juste ici :)")
+            .addField("n-aide", "Vous affiche Toutes les comand du bot")
+            .addField("n-infoserv", "le bot vous donne quelque information utile sur le serveur!")
+    message.channel.send(aide_embed)
+    break;
  
     case"serverlist":
     message.channel.send(client.guild.map(r => r.name + `| *${r.memberCount}* membre`))
-break;
+    break;
  }
 });
-
-client.login(process.env.TOKEN)
+ 
+client.login(process.env.TOKEN);
